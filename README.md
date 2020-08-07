c8bf3d97c572ec266d944e490422c876c709ad13
Nicolas <nicolas.estebe@agentil.com>
2020-08-07 09:39:11 +0200

    init

---
+++ README.md
@@ -0,0 +1,130 @@
+# README #
+
+JS library for SAP Business One Service Layer API.
+
+### How Install it ? ###
+
+
+### Usage ###
+
+```js
+const ServiceLayer = require("b1-service-layer")
+```
+
+Set your config object
+
+```js
+    var config = {
+        "host": "http://<server>",
+        "port": 50001,
+        "version": "v2",
+        "username": "b1User",
+        "password": "b1Password",
+        "company": "b1Database"
+    }
+```
+
+Create a new service layer session
+
+```javascript
+   var sl = new ServiceLayer()
+   await sl.createSession(config)
+```
+
+
+
+### Samples (with order object)
+
+```js
+/*Get an order by DocEntry*/
+var absoluteEntry = 100
+var order = await sl.get(`Orders(${absoluteEntry})`)
+
+/*
+return =>
+{
+   "@odata.context": "https://<server>:50000/b1s/v2/$metadata#Orders/$entity",
+   "DocEntry": 100,
+   "DocNum": 10,
+   "DocType": "dDocument_Items",
+   "HandWritten": "tNO",
+   "Printed": "psNo",
+   "DocDate": "2016-04-04",
+   "DocDueDate": "2016-12-02",
+   "CardCode": "customerCode",
+   "CardName": "Be... } 
+
+  OR (if error)
+  { error: true, message: "error message" }  
+*/
+
+           
+/*Get open orders by DocNum*/
+var list = await sl.get("Orders?$filter=DocumentStatus eq  'bost_Open'")
+
+/*
+return =>
+ {"@odata.context": "https://<server>:50000/b1s/v2/$metadata#Orders",
+   "value": [
+      {
+         "DocEntry": 8050,
+         "DocNum": 3715, ...
+      },
+      {
+         "DocEntry": 8051,
+         "DocNum": 3716, ...
+      },
+      ...
+      ],
+  "@odata.nextLink": "Orders?$filter=DocumentStatus%20eq%20%20%27bost_Open%27&$skip=20"
+ }
+      
+  OR (if error)
+  { error: true, message: "error message" }  
+*/
+
+
+/*Create new order*/
+var data = {
+    DocDate:'2020-01-01',
+    CardCode:'customerCode1',
+    DocumentLines: [
+        {ItemCode:"1122-22", Quantity: 1}
+    ],...
+}
+    
+var newOrder = await sl.post("Orders", data)
+
+
+/*Update new order*/
+var data = {
+    DocDueDate:'2020-02-01',
+}
+var newOrderUpdated = await sl.patch("Orders", data)
+
+/*Get all order without pagination (@odata.nextLink) */
+var allOrders = await sl.find("Orders")
+var allOrders = await sl.find("Orders?$filter=DocumentStatus eq  'bost_Open'")
+                
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,236 @@
+
+const axios = require('axios');
+const moment = require('moment');
+const https = require('https');
+
+class ServiceLayer {
+
+    constructor() {
+        this.instance = null
+        this.sessionTimeout = 0
+        this.startSessionTime = null
+        this.endSessionTime = null
+        this.config = null
+
+    }
+
+
+    /**
+      *Create a new session
+      *config object: {host, company, password, username}
+      */
+    async createSession(config, debug = false) {
+        this.config = config
+        axios.defaults.withCredentials = true;
+
+
+        if (config.host.slice(-1) == "/") {
+            config.host = config.host.substring(0, config.host.length - 1);
+        }
+
+        if (config.port) {
+            this.instance = axios.create({
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false
+                }),
+                baseURL: config.host + ":" + config.port + "/b1s/v2/"
+            });
+        } else {
+            this.instance = axios.create({
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false
+                }),
+                baseURL: config.host + "/b1s/v2/"
+            });
+        }
+
+
+        var result = await this.instance.post('Login', {
+            "CompanyDB": config.company,
+            "Password": config.password,
+            "UserName": config.username
+        });
+
+        this.instance.defaults.headers.common['Cookie'] = 'B1SESSION=' + result.data.SessionId + ';CompanyDB=' + config.company
+        if (debug) {
+            console.log(this.instance.defaults.headers.common['Cookie'])
+        }
+
+
+        this.sessionTimeout = result.data.SessionTimeout
+        this.startSessionTime = new moment();
+        this.endSessionTime = new moment();
+        this.endSessionTime.add(this.sessionTimeout - 1, 'minutes')
+
+    }
+
+    /**
+     * Refresh session if expired
+     */
+    async refreshSession() {
+
+
+        if (this.startSessionTime.isAfter(this.endSessionTime)) {
+
+            await this.createSession(this.config)
+        }
+
+    }
+
+    /**
+     * Simple service layer query (GET Method)
+     * 
+     */
+    async query(q) {
+        await this.refreshSession()
+        var result = await this.instance.get(q)
+        return result.data;
+    }
+
+    /**
+     * Find Ressource
+     * @param {String} query 
+     * (eg: ProductionOrders?$select=AbsoluteEntry, DocumentNumber)
+     */
+    async find(query) {
+
+        await this.refreshSession()
+
+        var result = []
+        var request = await this.query(query)
+        result = result.concat(request.value)
+
+        if (request["@odata.nextLink"]) {
+            request = await this.query(request["@odata.nextLink"])
+            result = result.concat(request.value)
+
+
+            while (request["@odata.nextLink"]) {
+                request = await this.query(request["@odata.nextLink"])
+                result = result.concat(request.value)
+            }
+
+        }
+        return result;
+
+    }
+
+
+    /**
+     * Get Ressource (eg Orders(10))
+     */
+    async get(ressource) {
+        try {
+            await this.refreshSession()
+            var result = await this.instance.get(ressource)
+            return result.data;
+        } catch (error) {
+
+            if (error.response) {
+                console.log("ERROR RESPONSE SERVICE LAYER:")
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+                return { error: true, message: error.response.data }
+            } else if (error.request) {
+                console.log("ERROR REQUEST")
+                return { error: true, message: "ERROR REQUEST" }
+
+            } else {
+                // Something happened in setting up the request and triggered an Error
+                console.log('Error', error.message);
+                return { error: true, message: error.message }
+            }
+        }
+    }
+    /**
+     * Update Ressource
+     */
+    async put(ressource, data) {
+        try {
+            await this.refreshSession()
+            var result = await this.instance.put(ressource, data)
+            return result.data;
+        } catch (error) {
+
+            if (error.response) {
+                console.log("ERROR RESPONSE SERVICE LAYER:")
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+                return { error: true, message: error.response.data }
+            } else if (error.request) {
+                console.log("ERROR REQUEST")
+                return { error: true, message: "ERROR REQUEST" }
+
+            } else {
+                // Something happened in setting up the request and triggered an Error
+                console.log('Error', error.message);
+                return { error: true, message: error.message }
+            }
+        }
+    }
+    /**
+     * Update Ressource partially
+     */
+    async patch(ressource, data) {
+        try {
+            await this.refreshSession()
+            var result = await this.instance.patch(ressource, data)
+            return result.data;
+        } catch (error) {
+
+            if (error.response) {
+                console.log("ERROR RESPONSE SERVICE LAYER:")
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+                return { error: true, message: error.response.data }
+            } else if (error.request) {
+                console.log("ERROR REQUEST")
+                return { error: true, message: "ERROR REQUEST" }
+
+            } else {
+                // Something happened in setting up the request and triggered an Error
+                console.log('Error', error.message);
+                return { error: true, message: error.message }
+            }
+
+        }
+
+    }
+    /**
+     * Create ressource
+     * 
+     */
+    async post(ressource, data) {
+        try {
+            await this.refreshSession()
+            var result = await this.instance.post(ressource, data)
+            return result.data;
+        } catch (error) {
+
+            if (error.response) {
+                console.log("ERROR RESPONSE SERVICE LAYER:")
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+                return { error: true, message: error.response.data }
+            } else if (error.request) {
+                console.log("ERROR REQUEST")
+                return { error: true, message: "ERROR REQUEST" }
+
+            } else {
+                // Something happened in setting up the request and triggered an Error
+                console.log('Error', error.message);
+                return { error: true, message: error.message }
+            }
+        }
+    }
+
+
+}
+
+module.exports = ServiceLayer
+
+
